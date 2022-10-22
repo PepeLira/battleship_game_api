f3b9f9e033e3126fa32c30f1bf4e882a8926542d
PepeLira <jflira@miuandes.cl>
2022-10-22 16:31:13 -0300

    fix and model relations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,5 @@
 class Board < ApplicationRecord
     belongs_to :game, dependent: :destroy
+    has_many :coordinates, dependent: :destroy
+    has_many :boats, dependent: :destroy
 end
--- app/models/player_room.rb
+++ app/models/player_room.rb
@@ -1,4 +1,5 @@
 class PlayerRoom < ApplicationRecord
   belongs_to :player, dependent: :destroy
   belongs_to :room, dependent: :destroy
+  has_many :boats, dependent: :destroy
 end
